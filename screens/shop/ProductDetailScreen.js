0493e1343c6c5b9bc80de19bad176fb134d3dc7e
yonureker <yonureker@gmail.com>
2019-12-10 15:40:08 -0800

    first commit

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  Button,
+  StyleSheet
+} from "react-native";
+import { useSelector } from "react-redux";
+
+const ProductDetailScreen = props => {
+  const productId = props.navigation.getParam("productId");
+  const SelectedProduct = useSelector(state =>
+    state.products.availableProducts.find(product => product.id === productId)
+  );
+
+  return (
+    <View>
+      <Text>{SelectedProduct.title}</Text>
+    </View>
+  );
+
+  
+};
+
+ProductDetailScreen.navigationOptions = navData => {
+  return {
+    headerTitle: navData.navigation.getParam('productTitle')
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default ProductDetailScreen;
