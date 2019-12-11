6fa20e7be5df9a3e9add53cfa9f41f04fd60cf96
yonureker <yonureker@gmail.com>
2019-12-10 16:30:52 -0800

    add font caching

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -63,10 +63,12 @@ const styles = StyleSheet.create({
     height: "100%"
   },
   title: {
+    fontFamily: 'open-sans-bold',
     fontSize: 18,
     marginVertical: 4
   },
   price: {
+    fontFamily: 'open-sans',
     fontSize: 14,
     color: "#888"
   },
