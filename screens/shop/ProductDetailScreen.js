e0d33d8216c33db490dc099098ebdbd831e776de
yonureker <yonureker@gmail.com>
2019-12-10 16:20:38 -0800

    add view with image, button and text

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -9,16 +9,21 @@ import {
 } from "react-native";
 import { useSelector } from "react-redux";
 
+import Colors from '../../constants/Colors'
+
 const ProductDetailScreen = props => {
   const productId = props.navigation.getParam("productId");
-  const SelectedProduct = useSelector(state =>
+  const selectedProduct = useSelector(state =>
     state.products.availableProducts.find(product => product.id === productId)
   );
 
   return (
-    <View>
-      <Text>{SelectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image}source={{uri: selectedProduct.imageUrl}}/>
+      <Button color={Colors.primary} title="Add to Cart" onPress={() => {}}/>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 
   
@@ -30,6 +35,21 @@ ProductDetailScreen.navigationOptions = navData => {
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20
+  }, 
+  description: {
+    fontSize: 14,
+    textAlign: 'center'
+  }
+});
 
 export default ProductDetailScreen;
